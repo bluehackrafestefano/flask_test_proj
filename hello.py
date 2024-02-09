edc2b4fa21907cb76e27c13dbff344fcf2b6cb60
Rafe <stefanorafe@gmail.com>
2024-02-09 17:40:32 +0100

    updates

--- hello.py
+++ hello.py
@@ -7,4 +7,5 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3003)
+    # app.run(debug=True, port=3003)
+    app.run(host='0.0.0.0', port=80)
